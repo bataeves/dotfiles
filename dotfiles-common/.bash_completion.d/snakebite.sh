03d8fc07819583f4dced0503a3b47bd81fbadb96
Батаев Евгений <e.bataev@corp.mail.ru>
2017-04-13 14:22:14 +0300

    hls completion

--- dotfiles-common/.bash_completion.d/snakebite.sh
+++ dotfiles-common/.bash_completion.d/snakebite.sh
@@ -5,6 +5,15 @@ _snakebite()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
+    if [ ${cur} == "hls" ]; then
+        cur="snakebite"
+        prev="ls"
+    fi
+    if [ ${cur} == "hdu" ]; then
+        cur="snakebite"
+        prev="du"
+    fi
+
     if [[ ${prev} != "snakebite" ]]; then
         local IFS=$'\t\n'
         if [[ $cur != /* ]] ; then
